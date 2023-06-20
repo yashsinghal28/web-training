3d6421798080e4b6755912280ce5ce59b8d38899
yashsinghal28 <singhalyash74@gmail.com>
2023-06-21 00:25:35 +0530

    added carousel

--- main.js
+++ main.js
@@ -17,6 +17,21 @@ var swiper = new Swiper(".skills", {
 }
 });
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 const toggleButton = document.getElementsByClassName('toogle-button')[0]
 const navbarLinks = document.getElementsByClassName('nav-links')[0]
 
